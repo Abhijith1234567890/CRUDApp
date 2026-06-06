b59460b2165db5177b97fd68c261ff97804d113d
Abhijith <153102915+Abhijith1234567890@users.noreply.github.com>
2026-06-06 16:41:18 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,6 @@ import mongoose from 'mongoose'
 async function connectDB() {
   const mongoUri = process.env.MONGO_URI
 
-  if (!mongoUri) {
-    throw new Error('Please add MONGO_URI in your .env file')
-  }
-
   await mongoose.connect(mongoUri)
   console.log('MongoDB Atlas connected')
 }
