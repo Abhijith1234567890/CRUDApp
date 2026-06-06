6cb6291e362b248a8d9626fba02bbd39b9918a7a
Abhijith <153102915+Abhijith1234567890@users.noreply.github.com>
2026-06-06 17:20:30 +0530

    Update todoModel.js

--- server/models/todoModel.js
+++ server/models/todoModel.js
@@ -5,56 +5,23 @@ const todoSchema = new mongoose.Schema(
     title: {
       type: String,
       required: true,
+      maxlength: 200,
+      trim: true,
     },
     completed: {
       type: Boolean,
       default: false,
     },
   },
-  { timestamps: true },
+  { timestamps: true, versionKey: false },
 )
 
 todoSchema.set('toJSON', {
   transform(_document, todo) {
     todo.id = todo._id.toString()
     delete todo._id
-    delete todo.__v
     return todo
   },
 })
 
-const Todo = mongoose.model('Todo', todoSchema)
-
-export async function getAllTodos() {
-  return Todo.find().sort({ createdAt: 1 })
-}
-
-export async function createTodo(title) {
-  return Todo.create({
-    title: title.trim(),
-  })
-}
-
-export async function updateTodoById(id, title, completed) {
-  if (!mongoose.isValidObjectId(id)) {
-    return null
-  }
-
-  return Todo.findByIdAndUpdate(
-    id,
-    {
-      title: title.trim(),
-      completed: Boolean(completed),
-    },
-    { new: true },
-  )
-}
-
-export async function removeTodo(id) {
-  if (!mongoose.isValidObjectId(id)) {
-    return false
-  }
-
-  const todo = await Todo.findByIdAndDelete(id)
-  return Boolean(todo)
-}
+export default mongoose.model('Todo', todoSchema)
